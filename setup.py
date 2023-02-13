7fb317abd1e9b2ad764aa577a25c23867bf3d290
Stavros Theocharis <stavros.theocharis@squaredev.io>
2023-02-13 18:14:24 +0200

    Adjustments

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="easy_explain",
     packages=["easy_explain"],
-    version="0.1.1",
+    version="0.1.3",
     license="MIT",
     description="A library that helps to explain AI models in a really quick & easy way",
     author="Stavros Theocharis",
     author_email="stavrostheocharis@yahoo.gr",
     url="https://github.com/stavrostheocharis/stavrostheocharis",
-    download_url="https://github.com/stavrostheocharis/easy_explain/archive/refs/tags/0.1.1.tar.gz",
+    download_url="https://github.com/stavrostheocharis/easy_explain/archive/refs/tags/v0.1.1.tar.gz",
     keywords=[
         "explainable ai",
         "xai",
@@ -17,7 +17,7 @@ setup(
     ],  # Keywords that define your package best
     install_requires=["torch", "torchvision", "captum"],  # I get to this in a second
     classifiers=[
-        "Development Status :: Beta",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",

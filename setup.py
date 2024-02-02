9d2310d1e9c47f2457ba518e2bf7257aa88eb03e
Stavros Theocharis <stavrostheocharis@yahoo.gr>
2024-02-02 12:00:03 +0200

    Changed version and setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.md", "r").read()
 setup(
     name="easy_explain",
     packages=["easy_explain"],
-    version="0.3.0",
+    version="0.4.0",
     license="MIT",
     description="A library that helps to explain AI models in a really quick & easy way",
     long_description=long_description,
@@ -13,7 +13,7 @@ setup(
     author="Stavros Theocharis",
     author_email="stavrostheocharis@yahoo.gr",
     url="https://github.com/stavrostheocharis/stavrostheocharis",
-    download_url="https://github.com/stavrostheocharis/easy_explain/archive/refs/tags/v0.1.1.tar.gz",
+    download_url="https://github.com/stavrostheocharis/easy_explain/archive/refs/tags/v0.4.0.tar.gz",
     keywords=[
         "explainable ai",
         "xai",
@@ -36,7 +36,7 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )

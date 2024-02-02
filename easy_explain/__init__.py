3e1a252e080d0582631f5ff0642f099759d07672
Stavros Theocharis <stavrostheocharis@yahoo.gr>
2024-02-02 12:40:22 +0200

    Paths init fixing

--- easy_explain/__init__.py
+++ easy_explain/__init__.py
@@ -1,3 +1,4 @@
-from .methods import YOLOv8LRP, OcclusionExplain
+from .methods.lrp.yolov8 import YOLOv8LRP
+from .methods.occlusion import OcclusionExplain
 
 __all__ = ["YOLOv8LRP", "OcclusionExplain"]
